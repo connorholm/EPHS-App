8dda4ced1a4d3e9d416b49a783c3ee4efc261bc0
Jack Gschwendtner <jackgsch456@gmail.com>
2020-10-21 10:00:26 -0500

    announcement page text

--- EPHS App/AnnouncementsView.swift
+++ EPHS App/AnnouncementsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AnnouncementsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Announcemts Page").font(.title)
     }
 }
 
