71594c7836634b3d39177b1791447a69abc42573
rachelPark1 <60945786+rachelPark1@users.noreply.github.com>
2020-10-26 11:00:44 -0500

    updated the login view

--- EPHS App/LoginView.swift
+++ EPHS App/LoginView.swift
@@ -10,19 +10,63 @@ import SwiftUI
 struct LoginView: View {
     @ObservedObject var viewRouter: ViewRouter
     
+    @State var schoolid: String = ""
+    @State var password: String = ""
+    
     var body: some View {
         VStack {
+            Image("ephs")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.bottom, 75)
+            
+            SchoolidTextField(schoolid: $schoolid)
+            PasswordSecureField(password: $password)
+            
             Button(action: {
                 viewRouter.currentPage = "tutorial"
             }) {
-                Text("Login").font(.title)
+                Text("Login")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width:228, height: 60)
+                    .background(Color.red)
+                    .cornerRadius(35)
             }
         }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView(viewRouter: ViewRouter())
+    }
+}
+
+struct SchoolidTextField: View {
+    
+    @Binding var schoolid: String
+    
+    var body: some View {
+        TextField("School ID", text: $schoolid)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(Color(.systemGray6))
+            .cornerRadius(5.0)
+            .padding(.bottom, 25)
     }
 }
