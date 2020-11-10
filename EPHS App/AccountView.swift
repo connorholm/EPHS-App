4bd3a1fca44cad6395a01c9b405fa9008851346f
rachelPark1 <60945786+rachelPark1@users.noreply.github.com>
2020-11-10 09:47:04 -0600

    accountview structure

--- EPHS App/AccountView.swift
+++ EPHS App/AccountView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 struct AccountView: View {
     var body: some View {
         VStack {
-            Text("Account Page").font(.title)
-            
+            Text("Account Page")
+                .font(.title)
+            Image("account")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                .padding(.bottom, 20)
+            Text("(Name)")
+                .foregroundColor(Color.gray)
+                .padding(.bottom, 10)
+            Text("(School Id)")
+                .foregroundColor(Color.gray)
+                .padding(.bottom, 20)
+
             Spacer()
         }
     }
